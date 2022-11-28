c31d08d802ff69ad9e40c1786b5b03e60c6c2b03
AnderssonPeter <1308309+AnderssonPeter@users.noreply.github.com>
2022-11-28 16:22:37 +0100

    Initial commit

---
+++ TestSourceGenerator/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Xml.Linq;
+using Humanizer;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace TestSourceGenerator;
+
+[Generator]
+public class SourceGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+
+        var builder = new StringBuilder(1024);
+
+        builder.AppendLine(@$"using System;
+namespace {context.Compilation.Assembly.ContainingNamespace?.Name ?? context.Compilation.Assembly.Name};
+class AssemblyInformation {{
+    public string Name = ""{context.Compilation.AssemblyName}"";
+    public string HumanizedName = ""{context.Compilation.AssemblyName.Humanize()}"";
+}}
+");
+
+        context.AddSource("test", SourceText.From(builder.ToString(), Encoding.UTF8));
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+
+    }
+}
